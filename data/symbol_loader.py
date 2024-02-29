c850deade2f69ca10c7fb4502c9728829de12cc2
Shamith Perera <shamith.gsjp@gmail.com>
2024-02-29 06:53:25 +0530

    added csv save functionality

--- data/symbol_loader.py
+++ data/symbol_loader.py
@@ -1,6 +1,6 @@
 import json
 import requests
-
+import csv
 
 
 exchange_info_url = "https://api.binance.com/api/v3/exchangeInfo"
@@ -8,7 +8,19 @@ response = requests.get(exchange_info_url)
 data_json = json.loads(response.text)
 
 #print(data_json["symbols"])
+symbols = []
 
 for symbol in data_json["symbols"]:
 	if(symbol["quoteAsset"] == "BTC"):
-		print(symbol["symbol"])
+		a = symbol["symbol"]
+		symbol_data = [a,a[:-3],a[-3:]]
+		symbols.append(symbol_data)
+
+
+print(len(symbols))
+
+with open('dataset_btc.csv', 'w', newline='') as file:
+	writer = csv.writer(file, delimiter=',' ,quoting=csv.QUOTE_MINIMAL)
+	writer.writerow(['ticker','symbol','base_symbol'])
+	for i in symbols:
+		writer.writerow(i)
