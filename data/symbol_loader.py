c89a9fc78d8fc44af4c3e13797e28cc7ccdbf946
Emindu <emindu.perera@gmail.com>
2024-03-04 06:35:56 +0530

    symbol loading adding verification of active tikcer

--- data/symbol_loader.py
+++ data/symbol_loader.py
@@ -2,6 +2,7 @@ import json
 import requests
 import csv
 import sys
+import time
 
 
 exchange_info_url = "https://api.binance.com/api/v3/exchangeInfo"
@@ -14,9 +15,16 @@ print(sys.argv[1])
 for symbol in data_json["symbols"]:
 	if(symbol["quoteAsset"] == sys.argv[1]):
 		symbolTicker = symbol["symbol"]
+		time.sleep(2)
+		validity_check_url = "https://www.binance.com/en/trade/" + symbolTicker
+		validity = 0
+		response_validity = requests.get(validity_check_url)
+		print(response_validity.status_code)
+		if(response_validity.status_code == 200 ):
+			validity = 1
 		quoteAsset = symbol["quoteAsset"]
 		baseAsset = symbol["baseAsset"]
-		symbol_data = [symbolTicker, baseAsset , quoteAsset, 1]
+		symbol_data = [symbolTicker, baseAsset , quoteAsset, validity]
 		symbols.append(symbol_data)
 
 
